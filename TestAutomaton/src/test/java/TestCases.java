e7e66b5d0191fe4ed6cfe58e3f3e16809b55708f
Amith Viduranga <62999097+viduranga-bit@users.noreply.github.com>
2023-07-16 14:02:47 +0530

    added new test cases

--- TestAutomaton/src/test/java/TestCases.java
+++ TestAutomaton/src/test/java/TestCases.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -15,6 +16,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.example.Common.Constants;
 
@@ -22,87 +24,72 @@ public class TestCases {
 
     ChromeDriver driver;
 
-    @Test
-    public void verifyLoginWithValidCredentials() {
+    @BeforeMethod
+    public void setupConfigurations() {
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments(Constants.CHROME_ARGUMENT);
+        chromeOptions.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(chromeOptions);
-        driver.get(Constants.WEB_URL);
-        driver.manage().window().maximize();
+        driver.get("https://demo.actitime.com/login.do");
+    }
+
+    @Test
+    public void loginSuccess() {
         driver.findElement(By.id("username")).sendKeys("admin");
         driver.findElement(By.name("pwd")).sendKeys("manager");
+        driver.findElement(By.id("loginButton")).click();
+    }
 
+    public void loginAsAdmin(ChromeDriver driver){
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.name("pwd")).sendKeys("manager");
+        driver.findElement(By.id("loginButton")).click();
+    }
+
+    public void loginAsUser(ChromeDriver driver){
+        driver.findElement(By.id("username")).sendKeys("trainee");
+        driver.findElement(By.name("pwd")).sendKeys("trainee");
         driver.findElement(By.id("loginButton")).click();
     }
 
     @Test
-    public void verifyLoginWithIncorrectUserName() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(chromeOptions);
-
-        driver.get(Constants.WEB_URL);
-        driver.manage().window().maximize();
+    public void failedLogin() {
         driver.findElement(By.id("username")).sendKeys("User");
-        driver.findElement(By.name("pwd")).sendKeys("manager");
-
-        driver.findElement(By.id("loginButton")).click();
-    }
-
-    @Test
-    public void verifyLoginWithIncorrectPassword() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(chromeOptions);
-
-        driver.get(Constants.WEB_URL);
-        driver.manage().window().maximize();
-        driver.findElement(By.id("username")).sendKeys("admin");
-        driver.findElement(By.name("pwd")).sendKeys("Password");
-
+        driver.findElement(By.name("pwd")).sendKeys("user");
         driver.findElement(By.id("loginButton")).click();
     }
 
     @Test
     public void reportsDashboard() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(chromeOptions);
+        this.loginAsAdmin(driver);
 
-        driver.get(Constants.WEB_URL);
-        driver.manage().window().maximize();
-        driver.findElement(By.id("username")).sendKeys("admin");
-        driver.findElement(By.name("pwd")).sendKeys("manager");
-
-        driver.findElement(By.id("loginButton")).click();
-
-        // Wait for the login to complete and navigate to the desired URL
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.urlContains("/user/submit_tt.do"));
 
-        driver.findElement(By.id("container_reports")).click();
+        driver.findElement(By.id("contaier_reports")).click();
+    }
+
+    @Test
+    public void captureScreenshotForFaildTest() {
+        this.loginAsAdmin(driver);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("/user/submit_tt.do"));
+
+        driver.findElement(By.id("fail-test")).click();
     }
 
     @AfterMethod
-    public void captureFailureScreenshot(ITestResult result) {
+    public void captureScreenshotOnFailure(ITestResult result) {
         if (result.getStatus() == ITestResult.FAILURE) {
-            // Capture screenshot and perform necessary actions for failure
-            String testName = result.getName();
-            String screenshotName = testName + ".png";
-            String screenshotPath = System.getProperty("user.dir") + File.separator + "test" + File.separator + screenshotName;
-            captureScreenshot(driver, screenshotPath);
-            // Perform additional actions like logging, reporting, etc.
-        }
-        driver.quit();
-    }
-
-    // Method to capture screenshot
-    public void captureScreenshot(WebDriver driver, String screenshotPath) {
-        try {
             File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            Files.copy(screenshotFile.toPath(), new File(screenshotPath).toPath(), StandardCopyOption.REPLACE_EXISTING);
-        } catch (Exception e) {
-            e.printStackTrace();
+            File destinationFile = new File("src/test/resources/screenshots/" + result.getName() + ".png");
+
+            try {
+                Files.copy(screenshotFile.toPath(), destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("Screenshot captured: " + destinationFile.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
